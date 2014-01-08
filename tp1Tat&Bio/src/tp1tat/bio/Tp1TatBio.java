79f6b5f6f922d916ba914e94a270563b38e57694
Nicolas <nicko466@gmail.com>
2014-01-08 14:17:14 +0100

    ajout base tp java

---
+++ tp1Tat&Bio/src/tp1tat/bio/Tp1TatBio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tp1tat.bio;
+
+/**
+ *
+ * @author nicko
+ */
+public class Tp1TatBio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
