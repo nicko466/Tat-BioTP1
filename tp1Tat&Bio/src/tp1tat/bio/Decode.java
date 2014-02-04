1e7df2de8051a72ead80461a66276780b6f7e0d7
morgan <morganbidois@gmail.com>
2014-02-04 21:07:10 +0100

    Nettoyage + Commentaire

--- tp1Tat&Bio/src/tp1tat/bio/Decode.java
+++ tp1Tat&Bio/src/tp1tat/bio/Decode.java
@@ -20,14 +20,14 @@ public class Decode {
     private int sizeSecretImage;
     private String nameSecretImage;
     private ShortPixmap sp;
-    private int endHeader = 159-15;
+    private int endHeader = 159 - 15;
 
     public Decode(String src) {
         try {
             sp = new ShortPixmap(src);
-            System.out.println("height img orig" + sp.height);
-            System.out.println("size img orig " + sp.size);
-            System.out.println("width img orig" + sp.width);
+//            System.out.println("height img orig" + sp.height);
+//            System.out.println("size img orig " + sp.size);
+//            System.out.println("width img orig" + sp.width);
 
             this.data = new short[sp.size];
             for (int i = 0; i < sp.size; i++) {
@@ -40,6 +40,9 @@ public class Decode {
         extractFlowData();
     }
 
+    /**
+     * Parse le header pour lire la taille de l'image cachée et son nom
+     */
     public void parseHeader() {
         int byte1 = getByte(data[0], data[1], data[2], data[3]);
         int byte2 = getByte(data[4], data[5], data[6], data[7]);
@@ -51,10 +54,22 @@ public class Decode {
 
     }
 
+    /**
+     * Transforme le short en entrée en Int en ajoutant des 0
+     * @param val
+     * @return 
+     */
     public int byteToValueMasked(short val) {
         return val & 0x3;
     }
 
+    /**
+     * Parse le nom entre deux index
+     *
+     * @param start
+     * @param end
+     * @return
+     */
     public String parseName(int start, int end) {
         int i = start;
         String name = "";
@@ -64,10 +79,19 @@ public class Decode {
             name = name.concat(String.valueOf(c));
             i = i + 4;
         }
-        System.out.println("file export under name: " + name);
+        System.out.println("File exported under name: " + name);
         return name;
     }
 
+    /**
+     * Créer un entier à partir de 4 pair de bits
+     *
+     * @param v1
+     * @param v2
+     * @param v3
+     * @param v4
+     * @return
+     */
     public int getByte(short v1, short v2, short v3, short v4) {
         int vm1 = byteToValueMasked(v1);
         int vm2 = byteToValueMasked(v2);
@@ -76,12 +100,16 @@ public class Decode {
         return (vm1 << 6) + (vm2 << 4) + (vm3 << 2) + vm4;
     }
 
+    /**
+     * Lecture de l'image cachée et écriture dans le fichier qui porte son nom
+     */
     public void extractFlowData() {
         try {
+            //Créer un fichier dans lequel on va écrire l'image que l'on lit
             FileOutputStream fos = new FileOutputStream(nameSecretImage);
-            System.out.println(" size: "+sizeSecretImage);
-            for (int i = endHeader; i < sizeSecretImage*4 + endHeader; i+=4) {
-                int val= getByte(data[i],data[i+1],data[i+2],data[i+3]);
+            System.out.println("Content size: " + sizeSecretImage);
+            for (int i = endHeader; i < sizeSecretImage * 4 + endHeader; i += 4) {
+                int val = getByte(data[i], data[i + 1], data[i + 2], data[i + 3]);
                 fos.write(val);
             }
             fos.close();
--- tp1Tat&Bio/src/tp1tat/bio/Encode.java
+++ tp1Tat&Bio/src/tp1tat/bio/Encode.java
@@ -2,8 +2,6 @@ package tp1tat.bio;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,12 +15,18 @@ public class Encode {
     private String pathBaseImage;
     private String pathData2mask;
     private String pathOut;
-    private short sizeHeader = 0;
-    private final short StartFlow = 0;
     private ShortPixmap sp;
     private File fileToHide;
     private String nameFileToMask;
 
+    /**
+     * Constructeur de l'encodeur Encodage fait lors de cette construction
+     *
+     * @param pathBaseImage path de l'image dans laquelle on va cacher les
+     * données
+     * @param pathData2mask path de l'image qui va être cachée
+     * @param pathOut path de l'image de sortie
+     */
     public Encode(String pathBaseImage, String pathData2mask, String pathOut) {
         try {
             this.pathBaseImage = pathBaseImage;
@@ -30,12 +34,13 @@ public class Encode {
             this.nameFileToMask = "tatoumina1.jpg";
             this.pathOut = pathOut;
             this.sp = new ShortPixmap(pathBaseImage);
+
             //permet de créer le fichier pathOut s'il n'existe pas auparavant
             new File(pathOut);
+
             fileToHide = new File(pathData2mask);
             long size2mask = fileToHide.length();
-            System.out.println(" taille ficher a masqué " + size2mask);
-            sizeHeader = (short) (16 + pathOut.length() * Character.SIZE);
+            System.out.println("Taille ficher à masquer " + size2mask);
             encodeHeader(size2mask, this.nameFileToMask);
             encodeData();
         } catch (IOException ex) {
@@ -43,10 +48,16 @@ public class Encode {
         }
     }
 
+    /**
+     * Encode le header de l'image cachée : taille du contenu + nom
+     *
+     * @param size2mask taille de l'image qui est cachée
+     * @param name nom de l'image qui est cachée
+     */
     public void encodeHeader(long size2mask, String name) {
         int size = (int) size2mask;
 
-        System.out.println(" taille size2mask en long " + size2mask + " taille size2mask en int " + (short) ((int) size2mask));
+//        System.out.println("Taille size2mask en long " + size2mask + " taille size2mask en int " + (short) ((int) size2mask));
         encodeByte(0, (int) size2mask);
         encodeByte(12, size);
         encodeByte(8, size >> 8);
@@ -58,15 +69,19 @@ public class Encode {
 
     }
 
+    /**
+     * Encode le contenu de l'image
+     */
     public void encodeData() {
         FileInputStream fileToHideInputStream = null;
         try {
             fileToHideInputStream = new FileInputStream(fileToHide);
             int content;
-            int i =144;
+            //On commence à mettre le contenu à partir de l'octet 144
+            int i = 144;
             while ((content = fileToHideInputStream.read()) != -1) {
-                encodeByte(i, (short)content);
-                i = i +4;
+                encodeByte(i, (short) content);
+                i = i + 4;
             }
             //export data
             sp.write(pathOut);
@@ -76,22 +91,38 @@ public class Encode {
         }
     }
 
+    /**
+     * Encode une valeur à un endroit donné
+     *
+     * @param indexToWrite index à partir duquel on va caché une valeur
+     * @param valueToEncode valeur à cacher
+     */
     public void encodeByte(int indexToWrite, short valueToEncode) {
+        //On met les 2 bits de poids faibles de 4 octets à 0
         for (int i = indexToWrite; i < indexToWrite + 4; i++) {
             sp.data[i] = (short) (sp.data[i] >> 2);
             sp.data[i] = (short) (sp.data[i] << 2);
         }
+        //On encode pour chaque octet deux bits de la valeur, en fonction de l'octet le masque à appliquer change
         sp.data[indexToWrite + 3] += valueToEncode & 0b00000011;
         sp.data[indexToWrite + 2] += (valueToEncode & 0b00001100) >> 2;
         sp.data[indexToWrite + 1] += (valueToEncode & 0b00110000) >> 4;
         sp.data[indexToWrite] += (valueToEncode & 0b11000000) >> 6;
     }
 
+    /**
+     * Encode une valeur à un endroit donné
+     *
+     * @param indexToWrite index à partir duquel on va caché une valeur
+     * @param valueToEncode valeur à cacher
+     */
     public void encodeByte(int indexToWrite, int valueToEncode) {
+        //On met les 2 bits de poids faibles de 4 octets à 0
         for (int i = indexToWrite; i < indexToWrite + 4; i++) {
             sp.data[i] = (short) (sp.data[i] >> 2);
             sp.data[i] = (short) (sp.data[i] << 2);
         }
+        //On encode pour chaque octet deux bits de la valeur, en fonction de l'octet le masque à appliquer change
         sp.data[indexToWrite + 3] += valueToEncode & 0b00000011;
         sp.data[indexToWrite + 2] += (valueToEncode & 0b00001100) >> 2;
         sp.data[indexToWrite + 1] += (valueToEncode & 0b00110000) >> 4;
